022f2a6b0410d600785220e4ea4223a26ae303be
soyyo5159 <soyyo5159@hotmail.com>
2017-12-19 22:17:57 -0300

    Agrego un ecualizador

--- js6/App.js
+++ js6/App.js
@@ -1,10 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 //export class App extends React.Component
+
+let audioStreamPromise=navigator.mediaDevices.getUserMedia({audio: true, video:false})
+
 export class App extends React.Component{
 
     constructor(props){
+        
         super(props);
+        this.analyser=null;
         this.state={
             address:""
         }
@@ -19,14 +24,56 @@ export class App extends React.Component{
             oscillator.frequency.value=args.frecuencia;
             oscillator.connect(audioContext.destination);
             oscillator.start();
-            oscilator.stop(audioContext.currentTime+2);
-            
+            oscillator.stop(audioContext.currentTime+2);
+        })
 
+        audioStreamPromise.then((stream)=>{
+            
+            let audioContext=new AudioContext();
+            let source=audioContext.createMediaStreamSource(stream);
+            var analyser = audioContext.createAnalyser();
+            source.connect(analyser);
+            analyser.fftSize = 2048;
+            this.analyser=analyser;
+            
+            
         })
     }
 
+    componentDidMount(){
+        let canvas=document.getElementById("canvas");
+        let ctx=canvas.getContext("2d");
+        let dataArray = null;
+        ctx.strokeStyle="black";
+        ctx.fillStyle="blue";
+        let f = (t)=>{
+            window.requestAnimationFrame(f)
+            if(this.analyser==null) return;
+
+            if(dataArray==null) dataArray=new Uint8Array(this.analyser.frequencyBinCount); 
+ 
+            this.analyser.getByteFrequencyData(dataArray);
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            
+            ctx.fillRect(0,0,300,300);
+            ctx.beginPath();
+            ctx.moveTo(0,300);
+            //dataArray=[0,0.5,0.3];
+            for(let i=0;i<dataArray.length;i++){
+                ctx.lineTo(i/dataArray.length*300,300-dataArray[i]/256*300);
+            }
+            ctx.stroke();
+
+            
+        };
+        window.requestAnimationFrame(f);
+    }
+
     render(){
-        return <span>hola, conectate entrando a {this.state.address}</span>;
+        return <span>
+            <span>hola, conectate entrando a {this.state.address}</span>
+            <canvas id="canvas" width="300" height="300"></canvas>
+        </span>
     }
     
 }
